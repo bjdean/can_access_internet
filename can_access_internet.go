3dd9cc15960a42e0fbc45d3041b9c862f6d44481
Bradley Dean <bjdean@bjdean.id.au>
2013-06-02 22:16:39 +1000

    go fmt

--- can_access_internet.go
+++ can_access_internet.go
@@ -1,27 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"github.com/bjdean/gonetcheck"
 	"os"
-	"fmt"
 )
 
 // Check if internet access is possible
 // If successful exit with 0
 // If internet access is detected as down exit with 1
 // If some other error occurs print the error and exit with 2
-func main () {
+func main() {
 	can_access_internet, err_list := gonetcheck.CheckInternetAccess()
 	switch err_list {
-		case nil:
-			switch can_access_internet {
-				case true: os.Exit(0)
-				default: os.Exit(1)
-			}
+	case nil:
+		switch can_access_internet {
+		case true:
+			os.Exit(0)
 		default:
-			fmt.Println(
-				"Error(s) returned by gonetcheck.CheckInternetAccess:",
-				err_list)
-			os.Exit(2)
+			os.Exit(1)
+		}
+	default:
+		fmt.Println(
+			"Error(s) returned by gonetcheck.CheckInternetAccess:",
+			err_list)
+		os.Exit(2)
 	}
 }
