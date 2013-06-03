c5bc6f72ca8720d9efd4cd8ff2b9b90da2793754
Bradley Dean <bjdean@bjdean.id.au>
2013-06-04 00:26:54 +1000

    Add TCP tests - this code now defines the default test urls and tcp addresses

--- can_access_internet.go
+++ can_access_internet.go
@@ -6,12 +6,34 @@ import (
 	"os"
 )
 
+// The default test URLS
+var testUrls = []string{
+	"http://www.google.com/",
+	"http://www.bing.com/",
+	"http://www.microsoft.com/",
+	"http://www.apple.com/",
+	"http://yahoo.com/",
+	"http://www.unimelb.edu.au/",
+	"http://abc.net.au/",
+	"http://www.monash.edu.au/",
+	"http://bbb.co.uk/",
+}
+
+// The default test TCP addresses
+var testTcpAddrs = []string{
+	"www.google.com:443",
+	"www.example.com:80",
+	"tty.freeshell.net:22",
+}
+
 // Check if internet access is possible
 // If successful exit with 0
 // If internet access is detected as down exit with 1
 // If some other error occurs print the error and exit with 2
 func main() {
-	canAccessInternet, errList := gonetcheck.CheckInternetAccess()
+	canAccessInternet, errList := gonetcheck.CheckInternetAccess(
+		testUrls,
+		testTcpAddrs)
 	switch errList {
 	case nil:
 		switch canAccessInternet {
