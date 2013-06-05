a009290c95a044d7571737abb600e6ae7ee398a5
Bradley Dean <bjdean@bjdean.id.au>
2013-06-06 01:09:50 +1000

    Add command line options -cfg and -[v|verbose], implement json config

--- can_access_internet.go
+++ can_access_internet.go
@@ -4,26 +4,17 @@ import (
 	"fmt"
 	"github.com/bjdean/gonetcheck"
 	"os"
+	"flag"
+	"encoding/json"
 )
 
-// The default test URLS
-var testUrls = []string{
-	"http://www.google.com/",
-	"http://www.bing.com/",
-	"http://www.microsoft.com/",
-	"http://www.apple.com/",
-	"http://yahoo.com/",
-	"http://www.unimelb.edu.au/",
-	"http://abc.net.au/",
-	"http://www.monash.edu.au/",
-	"http://bbb.co.uk/",
-}
-
-// The default test TCP addresses
-var testTcpAddrs = []string{
-	"www.google.com:443",
-	"www.example.com:80",
-	"tty.freeshell.net:22",
+// Command-line flags
+var progCfgFilepath string
+var verbose bool
+func init() {
+	flag.StringVar(&progCfgFilepath, "cfg", "", "configuration override")
+	flag.BoolVar(&verbose, "verbose", false, "verbose output")
+	flag.BoolVar(&verbose, "v", false, "verbose output")
 }
 
 // Check if internet access is possible
@@ -31,9 +22,23 @@ var testTcpAddrs = []string{
 // If internet access is detected as down exit with 1
 // If some other error occurs print the error and exit with 2
 func main() {
+	flag.Parse()
+
+	if verbose {
+		gonetcheck.DEBUG = gonetcheck.DBG_VERBOSE
+	}
+
+	var cfg progCfg
+	switch progCfgFilepath {
+		case "":
+			cfg = defaultProcCfg
+		default:
+			cfg = readJsonCfg(progCfgFilepath)
+	}
+
 	canAccessInternet, errList := gonetcheck.CheckInternetAccess(
-		testUrls,
-		testTcpAddrs)
+		cfg.Urls,
+		cfg.TcpAddrs)
 	switch errList {
 	case nil:
 		switch canAccessInternet {
@@ -48,4 +53,48 @@ func main() {
 			errList)
 		os.Exit(2)
 	}
+}
+
+func readJsonCfg (progCfgFilepath string) progCfg {
+	jsonFh, jsonFhErr := os.Open(progCfgFilepath)
+	if jsonFhErr != nil { panic(jsonFhErr) }
+
+	fhStat, fhStatErr := jsonFh.Stat()
+	if fhStatErr != nil { panic(fhStatErr) }
+
+	jsonBytes := make( []byte, fhStat.Size() )
+	_, readErr := jsonFh.Read(jsonBytes)
+	if readErr != nil { panic(readErr) }
+	jsonFh.Close()
+
+	var cfg progCfg
+	unmarshalErr := json.Unmarshal(jsonBytes, &cfg)
+	if unmarshalErr != nil { panic(unmarshalErr) }
+
+	return cfg
+}
+
+// Script configuration struct
+type progCfg struct {
+	Urls, TcpAddrs []string
+}
+
+// Default configuration
+var defaultProcCfg = progCfg{
+	Urls : []string{
+		"http://www.google.com/",
+		"http://www.bing.com/",
+		"http://www.microsoft.com/",
+		"http://www.apple.com/",
+		"http://yahoo.com/",
+		"http://www.unimelb.edu.au/",
+		"http://abc.net.au/",
+		"http://www.monash.edu.au/",
+		"http://bbb.co.uk/",
+	},
+	TcpAddrs : []string{
+		"www.google.com:443",
+		"www.example.com:80",
+		"tty.freeshell.net:22",
+	},
 }
